5be5aaff8359d7a5732462eae3376f2e6ae067aa
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-01-10 19:51:13 +0500

    No Motion in Form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,7 +4,6 @@ import { IoIosArrowBack } from "react-icons/io";
 import { nanoid } from "nanoid";
 import { ProductContext } from "../utils/Context";
 import { toast, Zoom } from "react-toastify";
-import { motion } from "motion/react";
 const Form = () => {
       const { product, setProduct } = useContext(ProductContext);
       const [title, setTitle] = useState("");
@@ -42,29 +41,29 @@ const Form = () => {
             <form className="w-screen h-screen flex justify-center items-center" action="" onSubmit={handleSubmit}>
                   <div className="w-full h-full flex flex-col bg-zinc-100 rounded-lg p-4 shadow-sm relative">
                         <button onClick={goBack}>
-                              <motion.div whileTap={{ scale: 0.6 }} className="rounded-full bg-black text-white w-11 h-11 flex items-center justify-center absolute z-20 top-4 left-4">
+                              <div className="rounded-full bg-black text-white w-11 h-11 flex items-center justify-center absolute z-20 top-4 left-4">
                                     <IoIosArrowBack size={"1.3rem"} />
-                              </motion.div>
+                              </div>
                         </button>
                         <h2 className="text-black font-bold text-lg mt-24">Add a new Product</h2>
                         <div className="mt-4">
                               <label className="text-black" htmlFor="title">
                                     Title
                               </label>
-                              <motion.input whileFocus={{ scale: 1.058 }} onChange={(e) => setTitle(e.target.value)} value={title} name="title" id="title" placeholder="Product's Title" className="w-full bg-white rounded-md border-gray-300 outline-black p-3 text-lg text-black" type="text" />
+                              <input onChange={(e) => setTitle(e.target.value)} value={title} name="title" id="title" placeholder="Product's Title" className="w-full bg-white rounded-md border-gray-300 outline-black p-3 text-lg text-black" type="text" />
                         </div>
                         <div className="mt-4">
                               <label className="text-black" htmlFor="description">
                                     Discription
                               </label>
-                              <motion.textarea whileFocus={{ scale: 1.058 }} onChange={(e) => setDescription(e.target.value)} value={description} name="description" placeholder="Product Discription goes here..." className="w-full outline-black bg-white rounded-md border-gray-300 p-3 text-lg text-black px-2 py-1" id="description" />
+                              <textarea onChange={(e) => setDescription(e.target.value)} value={description} name="description" placeholder="Product Discription goes here..." className="w-full outline-black bg-white rounded-md border-gray-300 p-3 text-lg text-black px-2 py-1" id="description" />
                         </div>
                         <div className="mt-4 flex flex-row space-x-2">
                               <div className="flex-1">
                                     <label className="text-black" htmlFor="image">
                                           Image url
                                     </label>
-                                    <motion.input whileFocus={{ scale: 1.058 }} onChange={(e) => setImage(e.target.value)} value={image} placeholder="image url" className="w-full outline-black invalid:outline-red-900 bg-white rounded-md border-gray-300 p-3 text-lg text-black px-2 py-1" id="image" type="url" />
+                                    <input onChange={(e) => setImage(e.target.value)} value={image} placeholder="image url" className="w-full outline-black invalid:outline-red-900 bg-white rounded-md border-gray-300 p-3 text-lg text-black px-2 py-1" id="image" type="url" />
                               </div>
                         </div>
                         <div className="mt-4 flex flex-row space-x-2">
@@ -72,19 +71,19 @@ const Form = () => {
                                     <label className="text-black" htmlFor="zip">
                                           Price
                                     </label>
-                                    <motion.input whileFocus={{ scale: 1.058 }} onChange={(e) => setPrice(e.target.value)} value={price} placeholder="$" className="w-full bg-white rounded-md border-gray-300 p-3 text-lg outline-black text-black px-2 py-1" id="zip" type="number" />
+                                    <input onChange={(e) => setPrice(e.target.value)} value={price} placeholder="$" className="w-full bg-white rounded-md border-gray-300 p-3 text-lg outline-black text-black px-2 py-1" id="zip" type="number" />
                               </div>
                               <div className="flex-1">
                                     <label className="text-black" htmlFor="category">
                                           Category
                                     </label>
-                                    <motion.input whileFocus={{ scale: 1.058 }} onChange={(e) => setCategory(e.target.value)} value={category} placeholder="category" className="w-full bg-white rounded-md border-gray-300 p-3 text-lg outline-black text-black px-2 py-1" id="category" type="text" />
+                                    <input onChange={(e) => setCategory(e.target.value)} value={category} placeholder="category" className="w-full bg-white rounded-md border-gray-300 p-3 text-lg outline-black text-black px-2 py-1" id="category" type="text" />
                               </div>
                         </div>
                         <div className="mt-4 flex justify-end">
-                              <motion.button whileTap={{ scale: 2 }} className="bg-green-200 rounded-lg text-green-800  px-5 py-2" type="submit">
+                              <button className="bg-green-200 rounded-lg text-green-800  px-5 py-2" type="submit">
                                     Add Now
-                              </motion.button>
+                              </button>
                         </div>
                   </div>
             </form>
